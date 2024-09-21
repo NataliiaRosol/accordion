312f2f44791baa2eea22f7f904c0f3884bf97ea2
NataliiaRosol <93214918+NataliiaRosol@users.noreply.github.com>
2024-09-21 11:28:00 +0300

    fixed styles

--- src/components/accordeon/Accordeon.jsx
+++ src/components/accordeon/Accordeon.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import './styles.css'
 import data from "./data";
 
@@ -8,7 +8,7 @@ export default function Accordeon(){
 
   const [selected, setSelected] = useState(null);
   const [enableMultiSelection, setEnableMultiSelection] = useState(false);
-  const [multipleSelected, setMultipleSelected] = useState([])
+  const [multipleSelected, setMultipleSelected] = useState([]);
 
   function handleSingleSelection(getId){
     setSelected(getId === selected ? null : getId)
@@ -25,25 +25,28 @@ export default function Accordeon(){
   }
   return(
     <div className="wrapper">
-      <button onClick={()=> setEnableMultiSelection(!enableMultiSelection)}>{enableMultiSelection ? 'Enable multiselection' : 'Multiselection enabled'}</button>
+      <div className="container">
+      <button onClick={ ()=> setEnableMultiSelection(!enableMultiSelection)} className='enableMultiSelectionBtn'>{enableMultiSelection ? 'Multiselection enabled' :  'Enable multiselection'}</button>
       <div className="accordeon"> 
         {
           data && data.length > 0 ? 
-          data.map(item => (
-            <div className="item" key={item.id}>
-              <div onClick={enableMultiSelection ? ()=> handleMultiSelection(item.id) : ()=> handleSingleSelection(item.id)} className="title">
-                <h3>{item.question}</h3>
-                <span>+</span>
+            data.map(item => (
+              <div className="item" key={item.id}>
+                <div onClick={enableMultiSelection ? ()=> handleMultiSelection(item.id) : ()=> handleSingleSelection(item.id)} className="title">
+                  <h3>{item.question}</h3>
+                  <span>{selected === item.id || multipleSelected.indexOf(item.id) !== -1 ? '-' :  '+'}</span>
+                </div>
+                
+                {
+                  selected === item.id || multipleSelected.indexOf(item.id) !==-1 ? 
+                    <div className="content">{item.answer}</div>
+                    : null
+                }
               </div>
-              {
-                selected === item.id || multipleSelected.indexOf(item.id) !==-1 ? 
-                <div className="content">{item.answer}</div>
-                : null
-              }
-            </div>
-          ) )
+            ) )
           : <h2>No data found</h2>
         }
+      </div>
       </div>
     </div>
   )
